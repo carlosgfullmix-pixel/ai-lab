25945bd1ec0260c40d4e29012ae14ed3b6a5375b
Carlos Galarco <carlosgfullmix@gmail.com>
2026-05-06 18:00:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT;
+// IMPORTANTE: fallback de puerto
+const PORT = process.env.PORT || 3000;
 
-// IMPORTANTE: Railway necesita este bind explícito
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port " + PORT);
-});
-
-// ruta obligatoria
 app.get("/", (req, res) => {
   res.status(200).send("Servidor OK 🚀");
+});
+
+// IMPORTANTE: bind correcto
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
 });
