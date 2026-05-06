3f5b8be78bc0a9d387771772940a9ba8988d6179
Carlos Galarco <carlosgfullmix@gmail.com>
2026-05-06 17:36:43 -0300

    Actualizar server.js

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
+// IMPORTANTE: Railway necesita esto
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(express.json());
-
+// rutas
 app.get("/", (req, res) => {
-  res.send("Servidor OK 🚀");
+  res.status(200).send("Servidor OK 🚀");
 });
 
-app.listen(PORT, () => {
+// IMPORTANTE: escuchar en 0.0.0.0
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Running on port " + PORT);
 });
